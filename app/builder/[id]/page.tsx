432aef2a214e0a915ba8fae4534b97c51caf80e6
carlesneri <carlesneri@hotmail.com>
2026-04-08 11:17:07 +0200

    Create main pages

---
+++ app/builder/[id]/page.tsx
@@ -0,0 +1,7 @@
+export async function BuilderIdPage() {
+  const id = 1
+
+
+  return <h1>Builder Id {id} Page</h1>
+
+}
---
+++ app/builder/page.tsx
@@ -0,0 +1,6 @@
+export async function BuilderPage() {
+
+
+  return <h1>Builder Page</h1>
+
+}
